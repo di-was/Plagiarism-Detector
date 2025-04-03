bb16ffd1bde27ac73f3a50bd075b85b66fc5fd02
Diwas Dahal <diwasdahal2006@gmail.com>
2025-04-03 12:53:55 +1100

    Base class definition - Naive Algorithm

---
+++ plagiarismDetector/Algorithms.cs
@@ -0,0 +1,21 @@
+namespace Algorithms
+{
+    public class Algorithm
+    {
+        public virtual String Name { get; set; }
+        public virtual String Description { get; set; }
+        public Algorithm() {
+            Name = "New Algorithm";
+            Description = "No Description";
+        }
+    }
+
+    public class NaiveAlgorithm : Algorithm
+    {
+        public NaiveAlgorithm()
+        {
+            Name = "Naive Algorithm";
+            Description = "Time Complexity : O(m*n)";
+        }
+    }
+}
